d4618caf1ff4ee43ad274ca76486716d461ee3ee
yuudj <yuudj@hotmail.com>
2023-05-24 13:16:10 -0300

    UPGRADE: - Influxdb 2.0 - Flux grafana datasource - Influx v2 telegraf connection - Upgrade dasboard to use flux query - Adding mqtt datasource

--- publisher/test.py
+++ publisher/test.py
@@ -8,4 +8,4 @@ mqttc.connect("mqtt", 1883, 60)
 mqttc.loop_start()
 while True:
     mqttc.publish("/casa/living/temp",random.randrange(-5, 35, 3))
-    time.sleep(3)# sleep for 10 seconds before next call
+    time.sleep(3)
